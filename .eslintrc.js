71d66be89b3c65f0174e83ee1f1406dca233a860
liaohui5 <liaohui5@foxmail.com>
2022-10-04 09:40:04 +0800

    update depends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,16 @@
 module.exports = {
   root: true,
-
   env: {
-    node: true
+    node: true,
   },
-
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-
+  // extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended"],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "@babel/eslint-parser",
   },
-
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
   },
-
-  overrides: [
-    {
-      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
-      env: {
-        jest: true
-      }
-    }
-  ]
 };
